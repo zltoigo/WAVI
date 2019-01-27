0f13f47ece2ea5642ece4a0c8ae71b4968956dcc
Alicia Hu <37424206+alicia-hu@users.noreply.github.com>
2019-01-27 09:07:03 -0500

    Add files via upload

--- final_webapp/app.py
+++ final_webapp/app.py
@@ -2,8 +2,10 @@ import RPi.GPIO as GPIO
 import time
 from flask import Flask, render_template, url_for, redirect
 
+# Set up flask
 app = Flask(__name__)
 
+# Set up IO pin out
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
     
@@ -20,56 +22,35 @@ time.sleep(2)
 
 @app.route("/")
 def index():
-   ##    ##GPIO.cleanup()
     return redirect(url_for("getDistance"))
 
 @app.route("/measure")
 def getDistance():
+    # send ultrasound ping
     GPIO.output(TRIG, True)
     time.sleep(0.00001)
     GPIO.output(TRIG, False)
+
     pulse_start = time.time()
     while GPIO.input(ECHO) == 0:
         pulse_start = time.time()
-        print("echo = 0")
-    pulse_end = time.time()
+
+    pulse_end = time.time(); 
     while GPIO.input(ECHO) == 1:
         pulse_end = time.time()
-        print("echo 1")
 
-
+    # calculate distance
     pulse_duration = pulse_end - pulse_start
-
     distance = pulse_duration * 17150
     distance = round(distance, 2)
     print "Distance:", distance, "cm"
 
-##    Buzzer = GPIO.PWM(BUZZ, 1100)
-##    Buzzer.start(50)
-##    
-##    if distance > 20 and distance < 70:
-##        Buzzer.ChangeFrequency(1500)
-##        time.sleep(0.5)
-##        Buzzer.stop()
-##        
-##    elif distance > 70 and distance < 150:
-##        Buzzer.ChangeFrequency(1300)
-##        time.sleep(0.5)
-##        Buzzer.stop()
-##
-##        
-##    if distance > 150 and distance < 300:
-##        Buzzer.ChangeFrequency(1000)
-##        time.sleep(0.5)
-##        Buzzer.stop()
-##
-##    time.sleep(0.5)
-    
+    #update web page
     templateData = {
        'distanceData': distance
     }
     return render_template('index.html', **templateData)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='0.0.0.0')
         
